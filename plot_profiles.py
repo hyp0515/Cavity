6625a0c3e4b7dc69bd22ee6815a9599b6d8c0960
hyp0515 <b102030019@g-mail.nsysu.edu.tw>
2024-08-01 14:56:06 +0800

    Backup

--- plot_profiles.py
+++ plot_profiles.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from cavity_data import target, cavity
+from cavity_data import cavity
 
-au = 14959787069100
+au   = 1.496e13
+SB   = 5.6704e-5
+mp   = 1.6726e-24
+kB   = 1.3807e-16
+Msun = 1.9884e33
+Lsun = 3.828e33
+G    = 6.6743e-8
+yr   = 3.156e7
 
 r_grid = np.logspace(np.log10(0.1), np.log10(200), 1000)
 
@@ -16,13 +23,20 @@ def surface_density(rc, sigmac, delta_gas, rcav, gamma = 1):
     )
     return density
 
+def surface_density_viscous(Lstar, Mstar, Mdot, phi=0.02, alpha=1e-3):
+    T_r = (phi*Lstar*Lsun/(8*np.pi*SB*(r_grid*au)**2))**(1/4)
+    sigma_vis = Mdot*(Msun/yr)*2*mp/(3*np.pi*alpha*kB*T_r)*np.sqrt(G*Mstar*Msun/(r_grid*au)**3)
+    return sigma_vis
 
 # Parameters
-name_list = list(target.keys())
-rc_list = cavity[r'$r_{c}$']
-sigmac_list = cavity[r'$\Sigma_{c}$']
-deltagas_list = cavity[r'$\delta_{gas}$']
-rcav_list = cavity[r'$r_{cav}$']
+name_list   = cavity['name']
+rc_list     = cavity['rc']
+sigmac_list = cavity['sigmac']
+deltag_list = cavity['deltag']
+rcavg_list  = cavity['rcavg']
+Lstar_list  = cavity['Lstar']
+Mstar_list  = cavity['Mstar']
+Mdot_list   = cavity['Mdot']
 
 
 
@@ -33,22 +47,26 @@ for i in range(len(rc_list)):
     if i == 5:
         sigma = surface_density(rc = rc_list[i],
                                 sigmac=sigmac_list[i],
-                                delta_gas=deltagas_list[i],
-                                rcav=rcav_list[i],
+                                delta_gas=deltag_list[i],
+                                rcav=rcavg_list[i],
                                 gamma=0.3)
     elif i == 16:
         sigma = surface_density(rc = rc_list[i],
                                 sigmac=sigmac_list[i],
-                                delta_gas=deltagas_list[i],
-                                rcav=rcav_list[i],
+                                delta_gas=deltag_list[i],
+                                rcav=rcavg_list[i],
                                 gamma=1.1)
     else:
         sigma = surface_density(rc = rc_list[i],
                                 sigmac=sigmac_list[i],
-                                delta_gas=deltagas_list[i],
-                                rcav=rcav_list[i],
+                                delta_gas=deltag_list[i],
+                                rcav=rcavg_list[i],
                                 gamma=1)
-    ax[i].plot(r_grid, sigma)
+    sigma_vis = surface_density_viscous(Lstar=Lstar_list[i],
+                                        Mstar=Mstar_list[i],
+                                        Mdot=Mdot_list[i])
+    ax[i].plot(r_grid, sigma, label = 'Andrews+11')
+    ax[i].plot(r_grid, sigma_vis, label = 'Manara+14')
     ax[i].set_title(name_list[i], fontsize = 16)
     ax[i].set_xlim((0.1, 200))
     ax[i].set_ylim((1e-6, 1e6))
@@ -57,6 +75,7 @@ for i in range(len(rc_list)):
     if i == 16:
         ax[i].set_xlabel('R [AU]', fontsize = 16)
         ax[i].set_ylabel(r'$\Sigma_{g} [g cm^{-2}]$', fontsize = 16)
+    ax[i].legend(fontsize = 16)
     
 
 plt.tight_layout()
--- plot_scatter.py
+++ plot_scatter.py
@@ -4,27 +4,27 @@ import pandas as pd
 from scipy.optimize import curve_fit
 
 from itertools import combinations
-from cavity_data import cavity
+from cavity_data import cavity, key_to_text
 
-# Creating a DataFrame
 df = pd.DataFrame(cavity)
 
-# Create a list of parameter names
 params = list(df.columns)
-params.remove(r'$r_{c}$')
-params.remove(r'$\Sigma_{c}$')
-params.remove(r'$Myr$')
-# params.remove(r'$\delta_{gas}$')
-# Generate all combinations of pairs of parameters
-param_combinations = list(combinations(params, 2))
+params.remove('name')
+params.remove('rc')
+params.remove('sigmac')
+# params.remove('age')
+# params.remove('deltad')
 
-# Calculate the number of combinations
+param_combinations = list(combinations(params, 2))
 num_combinations = len(param_combinations)
 
-# Create subplots
-fig, axes = plt.subplots(3, num_combinations // 3 + num_combinations % 3, figsize=(45, 20))
-
-# Flatten the axes array for easy iteration
+if num_combinations % 4 == 0:
+    if num_combinations % 3 == 0:
+        fig, axes = plt.subplots(6, num_combinations // 6 + num_combinations % 6, figsize=(40, 40))
+    else: 
+        fig, axes = plt.subplots(4, num_combinations // 4 + num_combinations % 4, figsize=(48, 30))
+else:
+    fig, axes = plt.subplots(3, num_combinations // 3 + num_combinations % 3, figsize=(45, 20))
 axes = axes.flatten()
 
 # Plot each combination
@@ -33,21 +33,20 @@ def linear_model(x, a, b):
 
 for i, (param1, param2) in enumerate(param_combinations):
     ax = axes[i]
-    ratio = np.array(df[r'$Myr$'])/(np.array(df[r'$M_{d}$'])/np.array(df[r'$\dot{M}$'])/333000)
-    ax.scatter(df[param1], df[param2], s=125, c=np.log10(ratio), cmap='rainbow')
+    # color_value = np.array(df['age'])/(np.array(df['Md'])/np.array(df['Mdot']))
+    color_value = np.array(df['Mdot'])/np.array(df['Md'])
+    ax.scatter(df[param1], df[param2], s=125, c=np.log10(color_value), cmap='rainbow')
     
     params, covariance = curve_fit(linear_model, np.log10(df[param1]), np.log10(df[param2]))
     a, b = params
     fitted_line = linear_model(np.log10(df[param1]), a, b)
     ax.plot(df[param1], 10**(fitted_line))
     
-    ax.set_xlabel(param1, fontsize=25)
-    ax.set_ylabel(param2, fontsize=25)
-    ax.set_title(f'{param1} vs {param2}', fontsize=25)
+    ax.set_xlabel(key_to_text[param1], fontsize=25)
+    ax.set_ylabel(key_to_text[param2], fontsize=25)
+    ax.set_title(f'{key_to_text[param1]} vs {key_to_text[param2]}', fontsize=25)
     ax.set_xscale('log')
     ax.set_yscale('log')
-    
-    
 
 # Remove any unused subplots
 for j in range(i + 1, len(axes)):
