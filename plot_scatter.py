d1a7199bb3a9acaf2a1d3564958a3bd36a34cbad
hyp0515 <b102030019@g-mail.nsysu.edu.tw>
2024-08-02 14:06:19 +0800

    Backup

--- plot_scatter.py
+++ plot_scatter.py
@@ -18,44 +18,65 @@ params.remove('sigmac')
 param_combinations = list(combinations(params, 2))
 num_combinations = len(param_combinations)
 
-if num_combinations % 4 == 0:
-    if num_combinations % 3 == 0:
-        fig, axes = plt.subplots(6, num_combinations // 6 + num_combinations % 6, figsize=(40, 40))
-    else: 
-        fig, axes = plt.subplots(4, num_combinations // 4 + num_combinations % 4, figsize=(48, 30))
-else:
-    fig, axes = plt.subplots(3, num_combinations // 3 + num_combinations % 3, figsize=(45, 20))
-axes = axes.flatten()
+# if num_combinations % 4 == 0:
+#     if num_combinations % 3 == 0:
+#         fig, axes = plt.subplots(6, num_combinations // 6 + num_combinations % 6, figsize=(40, 40))
+#     else: 
+#         fig, axes = plt.subplots(4, num_combinations // 4 + num_combinations % 4, figsize=(60, 30))
+# else:
+#     fig, axes = plt.subplots(3, num_combinations // 3 + num_combinations % 3, figsize=(45, 20))
+# axes = axes.flatten()
 
-# Plot each combination
-def linear_model(x, a, b):
-    return a * x + b
+# # Plot each combination
+# def linear_model(x, a, b):
+#     return a * x + b
 
-for i, (param1, param2) in enumerate(param_combinations):
-    ax = axes[i]
-    # color_value = np.array(df['age'])/(np.array(df['Md'])/np.array(df['Mdot']))
-    color_value = np.array(df['Mdot'])/np.array(df['Md'])
-    ax.scatter(df[param1], df[param2], s=125, c=np.log10(color_value), cmap='rainbow')
+# for i, (param1, param2) in enumerate(param_combinations):
+#     ax = axes[i]
+#     # color_value = np.array(df['Mdot'])/np.array(df['Md']/333000)
+#     color_value = np.array(df['Mdot'])/np.array(df['Mstar'])
+#     # color_value = np.array(df['deltag'])/np.array(df['deltad'])
+#     # color_value = df['Mdot']
+#     scatter = ax.scatter(df[param1], df[param2], s=125, c=np.log10(color_value), cmap='rainbow')
     
-    params, covariance = curve_fit(linear_model, np.log10(df[param1]), np.log10(df[param2]))
-    a, b = params
-    fitted_line = linear_model(np.log10(df[param1]), a, b)
-    ax.plot(df[param1], 10**(fitted_line))
+#     params, covariance = curve_fit(linear_model, np.log10(df[param1]), np.log10(df[param2]))
+#     a, b = params
+#     fitted_line = linear_model(np.log10(df[param1]), a, b)
+#     ax.plot(df[param1], 10**(fitted_line))
     
-    ax.set_xlabel(key_to_text[param1], fontsize=25)
-    ax.set_ylabel(key_to_text[param2], fontsize=25)
-    ax.set_title(f'{key_to_text[param1]} vs {key_to_text[param2]}', fontsize=25)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
+#     ax.set_xlabel(key_to_text[param1], fontsize=25)
+#     ax.set_ylabel(key_to_text[param2], fontsize=25)
+#     ax.set_title(f'{key_to_text[param1]} vs {key_to_text[param2]}', fontsize=25)
+#     ax.set_xscale('log')
+#     ax.set_yscale('log')
 
-# Remove any unused subplots
-for j in range(i + 1, len(axes)):
-    fig.delaxes(axes[j])
+# # Remove any unused subplots
+# for j in range(i + 1, len(axes)):
+#     fig.delaxes(axes[j])
 
-# Adjust layout
-plt.tight_layout()
+# cbar_ax = fig.add_axes([0.95, 0.05, 0.01, 0.9])
+# cbar = fig.colorbar(scatter, cax=cbar_ax, orientation='vertical')
+# cbar.set_label(r'Log($\dot{M}/M_{*}$)', fontsize=30)
+# cbar.ax.tick_params(labelsize=20)
+
+# # Adjust layout
+# fig.tight_layout(rect=[0, 0, 0.95, 1])
+# # plt.tight_layout()
+
+# # Show the plot
+# plt.savefig('scatter.pdf', transparent = True)
+# plt.close()
+
+import corner
+
+df_selected = df[params].apply(np.log10)
+figure = corner.corner(df_selected, labels=[key_to_text[param] for param in params],
+                       plot_contours=False,
+                       show_titles=True, title_kwargs={"fontsize": 12})
 
 # Show the plot
-plt.savefig('scatter.pdf', transparent = True)
-plt.close()
+# plt.show()
 
+# Save the plot
+figure.savefig('scatter_corner.pdf', transparent=True)
+plt.close()
